20c60ec6d8d9063aef1e3cca4084beeef30afb46
Zee Lavenhouse <Zeehlavenhouse@gmail.com>
2023-03-01 18:58:06 -0500

    Created simplified function

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,64 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# print("Day 1")
+# the_file = open("um-deliveries-day-1.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
 
 
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# print("Day 2")
+# the_file = open("um-deliveries-day-2.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-day-3.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+def produce_summary(file_name):
+
+    the_file = open(file_name)
+
+    day = file_name.split('-')
+    day = (day[-1].split('.'))[0]
+
+    print(f"Day {day}")
+
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon_name = words[0]
+        amount_melon = words[1]
+        total_cost_melon = words[2]
+
+        print(f"Delivered {amount_melon} {melon_name}s for a total of ${total_cost_melon}")
+
+    the_file.close()
+
+produce_summary("um-deliveries-day-2.txt")
+
